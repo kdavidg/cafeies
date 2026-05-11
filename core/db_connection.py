1ed80356d1eea15a8ed610752ec7853d81bc6ed9
kdavidg4 <davidgonzaga140@gmail.com>
2026-05-11 22:40:27 +0200

    Configurada conexión para Railway

--- core/db_connection.py
+++ core/db_connection.py
@@ -1,6 +1,8 @@
-# core/db_connection.py
+import os
+import pymongo
 from pymongo import MongoClient
 
-# Conectamos con tu MongoDB local
-client = MongoClient('mongodb://localhost:27017/')
+mongo_uri = os.getenv('MONGO_URL') or os.getenv('MONGODB_URI') or "mongodb://localhost:27017/"
+
+client = MongoClient(mongo_uri)
 db = client['cafe_db']
