53fde3dc4f5a275adf89ddef421475846437a6f0
kdavidg4 <davidgonzaga140@gmail.com>
2026-05-12 18:47:24 +0200

    Solucion definitiva

--- cafe_backend/urls.py
+++ cafe_backend/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 # Añadimos eliminar_pedido a la lista de importaciones
-from core.views import listar_productos, crear_pedido, listar_pedidos, auto_login, eliminar_pedido 
+from core.views import listar_productos, crear_pedido, listar_pedidos, eliminar_pedido 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
