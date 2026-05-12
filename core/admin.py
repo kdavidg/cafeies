70a4a3f54327c58df3a80b21221c0786abbdfe81
kdavidg4 <davidgonzaga140@gmail.com>
2026-05-12 16:31:16 +0200

    Tabla pedidos

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from .models import Producto
+from .models import Producto, Pedido
+
 
 admin.site.register(Producto)
+
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'total', 'franja_horaria', 'fecha')
+    list_filter = ('franja_horaria', 'fecha')
+    search_fields = ('usuario',)
