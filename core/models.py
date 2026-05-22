d288d236e04ff1c4467811f6fbd66d7141da3991
kdavidg4 <davidgonzaga140@gmail.com>
2026-05-22 16:24:36 +0200

    Codigo aleatorio pedido

--- core/models.py
+++ core/models.py
@@ -1,4 +1,17 @@
 from django.db import models
+import random
+import string
+
+def generar_codigo_pedido():
+    """Genera un código único de pedido: 2 letras + 3 números (ej: AB123)"""
+    while True:
+        letras = ''.join(random.choices(string.ascii_uppercase, k=2))
+        numeros = ''.join(random.choices(string.digits, k=3))
+        codigo = letras + numeros
+        
+        # Verificar que no exista
+        if not Pedido.objects.filter(codigo=codigo).exists():
+            return codigo
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
@@ -24,3 +37,9 @@ class Pedido(models.Model):
 
     def __str__(self):
         return f"{self.usuario} - {self.franja_horaria}"
+    
+    def save(self, *args, **kwargs):
+        # Si no tiene código, generar uno
+        if not self.codigo:
+            self.codigo = generar_codigo_pedido()
+        super().save(*args, **kwargs)
