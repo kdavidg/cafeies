20b8b7e8bfb58eda2a83de681063613de08dfc4b
kdavidg4 <davidgonzaga140@gmail.com>
2026-05-22 20:16:28 +0200

    Franjas horarias mejoradas

--- src/data/timeSlots.js
+++ src/data/timeSlots.js
@@ -1,8 +1,8 @@
 export const TIME_SLOTS = [
-  { time: '10:45', label: '10:45' },
-  { time: '11:00', label: '11:00', disabled: true },
-  { time: '11:15', label: '11:15' },
-  { time: '11:30', label: '11:30' },
-  { time: '14:00', label: '14:00' },
-  { time: '14:15', label: '14:15' },
+  { time: '10:45' },
+  { time: '11:00', disabled: true },
+  { time: '11:15' },
+  { time: '11:30' },
+  { time: '14:00' },
+  { time: '14:15' },
 ]
