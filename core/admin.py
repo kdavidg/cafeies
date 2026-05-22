798795be16ba44fc7540f506c2e3d2a78e85c92f
kdavidg4 <davidgonzaga140@gmail.com>
2026-05-22 16:50:26 +0200

    Columnas visibles

--- core/admin.py
+++ core/admin.py
@@ -3,13 +3,13 @@ from .models import Producto, Pedido
 
 @admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nombre', 'precio', 'categoria', 'emoji')
+    list_display = ('id', 'nombre', 'precio', 'categoria', 'emoji', 'descripcion', 'stock')
     list_filter = ('categoria',)
     search_fields = ('nombre',)
 
 @admin.register(Pedido)
 class PedidoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'usuario', 'total', 'estado', 'franja_horaria', 'fecha')
+    list_display = ('id', 'usuario', 'total', 'estado', 'franja_horaria', 'codigo','fecha')
     list_filter = ('estado', 'franja_horaria', 'fecha')
     list_editable = ('estado',)
     ordering = ('-fecha',)
