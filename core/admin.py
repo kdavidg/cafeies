2971e86c1b557947beb84371758e34cef41cc2b5
kdavidg4 <davidgonzaga140@gmail.com>
2026-05-27 18:28:53 +0200

    Franjas horas

--- core/admin.py
+++ core/admin.py
@@ -9,10 +9,16 @@ class ProductoAdmin(admin.ModelAdmin):
 
 @admin.register(Pedido)
 class PedidoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'usuario', 'total', 'estado', 'franja_horaria', 'codigo','fecha')
+    list_display = ('id', 'usuario', 'total', 'estado', 'franja_formateada', 'codigo','fecha')
     list_filter = ('estado', 'franja_horaria', 'fecha')
     list_editable = ('estado',)
     ordering = ('-fecha',)
+    
+    def franja_formateada(self, obj):
+        if obj.franja_horaria:
+            return f"{str(obj.franja_horaria.hora_inicio)[0:5]} - {str(obj.franja_horaria.hora_fin)[0:5]}"
+        return "-"
+    franja_formateada.short_description = "Franja Horaria"
 
 @admin.register(Usuario)
 class UsuarioAdmin(admin.ModelAdmin):
