f727668d51634f128c79bae7e17acbe568f1618f
kdavidg4 <davidgonzaga140@gmail.com>
2026-05-27 17:16:27 +0200

    Modelos en admin

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Producto, Pedido
+from .models import Producto, Pedido, Usuario, FranjasHorarias
 
 @admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
@@ -13,3 +13,14 @@ class PedidoAdmin(admin.ModelAdmin):
     list_filter = ('estado', 'franja_horaria', 'fecha')
     list_editable = ('estado',)
     ordering = ('-fecha',)
+
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('email', 'nombre', 'es_admin', 'fecha_registro')
+    list_filter = ('es_admin',)
+    search_fields = ('email', 'nombre')
+
+@admin.register(FranjasHorarias)
+class FranjasHorariasAdmin(admin.ModelAdmin):
+    list_display = ('hora_inicio', 'hora_fin', 'activa', 'max_pedidos')
+    list_filter = ('activa',)
